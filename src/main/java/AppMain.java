b99e5b8d5886783cc3aec041063eda1832349bd4
Carlos Perez <carlosperezgala@outlook.es>
2020-05-08 23:55:33 +0200

    Proyecto Base

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,2 +1,6 @@
 public class AppMain {
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/Fracciones.java
+++ src/main/java/Fracciones.java
@@ -1,2 +1,31 @@
 public class Fracciones {
+
+    private int den;
+    private int num;
+
+    public Fracciones(){
+        this.den = 0;
+        this.num = 0;
+    }
+
+    public Fracciones(int den, int num){
+        this.den = den;
+        this.num = num;
+    }
+
+    public int getDen() {
+        return den;
+    }
+
+    public void setDen(int den) {
+        this.den = den;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
